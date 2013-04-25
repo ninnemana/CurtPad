40d34711bda82e07495c0ff38655138d13658d9a
ninnemana <ninnemana@gmail.com>
2013-04-24 23:13:03 -0500

    init

---
+++ CURT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CURT
+//
+//  Created by Alex Ninneman on 4/24/13.
+//  Copyright (c) 2013 CURT Manufacturing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
